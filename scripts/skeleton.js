f67432678725c366a1be306c1f162b4985dd7933
Marjan Amiri <marjanamiri778@gmail.com>
2022-03-02 15:57:15 -0800

    css

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -1,1 +1,6 @@
 //footer and navbar 
+function loadSkeleton(){
+    console.log($('#navbarPlaceholder').load('./nav.html'));
+    console.log($('#footerPlaceholder').load('./footer.html'));
+}
+loadSkeleton();  //invoke the function
