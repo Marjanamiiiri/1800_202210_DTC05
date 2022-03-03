ad3b1f8f8020f07385fbe5208b86a75a869690ff
WallaceTrinh <WTrinh1@my.bcit.ca>
2022-03-03 12:18:02 -0800

    modifications to main

--- scripts/main.js
+++ scripts/main.js
@@ -27,4 +27,19 @@ function insertName() {
         }
     });
 }
+function insertName(){
+    firebase.auth().onAuthStateChanged(user =>{
+        if (user){
+            console.log(user.uid); // let me know who is the user that logged in to get the UID
+            currentUser = db.collection("users").doc(user.uid); // will go to the firestore and go to the document of the user
+            currentUser.get().then(userDoc=>{
+                //get the user name
+                var user_Name= userDoc.data().name;
+                console.log(user_Name);
+                $("#name-goes-here").text(user_Name); //jquery way
+                // document.getElementById("name-goes-here").innerText=user_Name;
+            })
+        }
+    })
+    }
 insertName();
