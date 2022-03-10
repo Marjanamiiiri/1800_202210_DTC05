b4d168adf99c13883dce01a460d4d3dc28f65dba
tgrigoruk <97200544+tgrigoruk@users.noreply.github.com>
2022-03-09 18:28:35 -0800

    added testing elements to athletes.html

--- scripts/athletes.js
+++ scripts/athletes.js
@@ -1,28 +1,39 @@
-function displayCards(collection) {
-    let cardTemplate = document.getElementById("athleteCardTemplate");
+function displayAthleteCards(collection) {
+  let cardTemplate = document.getElementById("athleteCardTemplate");
 
-    db.collection(collection).get()
-        .then(snap => {
-            var i = 1;
-            snap.forEach(doc => { //iterate thru each doc
+  db.collection(collection)
+    .get()
+    .then((snap) => {
+      var i = 1;
+      snap.forEach((doc) => {
+        //iterate thru each doc
 
-                // set some variable to data from database document
-                var name = doc.data().name;
-                var age = doc.data().age;
-                
-                // clone the template
-                let newcard = cardTemplate.content.cloneNode(true);
+        // set some variable to data from database document
+        var name = doc.data().name;
+        var age = doc.data().age;
 
-                //update elements of the clone
-                newcard.querySelector('.athlete-card-name').innerHTML = name;
-                newcard.querySelector('.thlete-card-age').innerHTML = age;
+        // clone the template
+        let newcard = cardTemplate.content.cloneNode(true);
 
+        //update elements of the clone
+        newcard.querySelector(".athlete-card-name").innerHTML = name;
+        newcard.querySelector(".thlete-card-age").innerHTML = age;
 
-                // attach to gallery
-                document.getElementById(collection + "-go-here").appendChild(newcard);
-                i++;
-            })
-        })
+        // attach to gallery
+        document.getElementById(collection + "-go-here").appendChild(newcard);
+        i++;
+      });
+    });
 }
 
-displayCards("athletes");
+// displayAthleteCard("athletes");
+function diplayAthletes(){
+    eventSelection = $('#event-selection').val();
+    console.log(eventSelection);
+}
+
+
+function setup() {
+  $("#get-event-selection").click(diplayAthletes);
+}
+$(document).ready(setup);
