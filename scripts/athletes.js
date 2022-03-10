5853352d3dbdae5586f62dbeaaedb94a1cb368f0
tgrigoruk <97200544+tgrigoruk@users.noreply.github.com>
2022-03-10 00:02:23 -0800

    added DB querying to sport-events & athletes pages

--- scripts/athletes.js
+++ scripts/athletes.js
@@ -1,38 +1,61 @@
-const urlParams = new URLSearchParams(window.location.search);
-const currEvent = urlParams(sportevent);
-
-function displayCards(collection) {
-  let cardTemplate = document.getElementById("athleteCardTemplate");
-
-   // use currEvent to query firestore and get all athletes for this event
-   // to populate templates with
-
-  db.collection(collection)
-    .get()
-    .then((snap) => {
-      var i = 1;
-      snap.forEach((doc) => {
-        // iterate thru each doc
-
-        // set some variable to data from database document
-        var name = doc.data().name;
-        var age = doc.data().age;
-
-        // clone the template
-        let newcard = cardTemplate.content.cloneNode(true);
-
-        // update elements of the clone
-        newcard.querySelector(".athlete-card-name").innerHTML = name;
-        newcard.querySelector(".thlete-card-age").innerHTML = age;
-
-        // attach to gallery
-        document.getElementById(collection + "-go-here").appendChild(newcard);
-        i++;
-      });
-    });
+// page url should look something like:
+// athletes.html?sport=Skating&gender=Women%27s&sportevent=1,000m
+function getEventInfo() {
+  const urlParams = new URLSearchParams(window.location.search);
+  return {
+    sport: urlParams.get("sport"),
+    gender: urlParams.get("gender"),
+    sportevent: urlParams.get("sportevent"),
+  };
 }
 
-// displayCards("athletes");
+function displayCards(collection) {
+  eventInfo = getEventInfo();
+  document.getElementById("athletes-title").innerHTML =
+    "Competitors in " + eventInfo.gender + " " + eventInfo.sportevent;
+
+  let cardTemplate = document.getElementById("athleteCardTemplate");
+  // var athletesRef = db.collection(collection);
+  // use currEvent to query firestore and get all athletes for this event
+  //  var query = athletesRef.where("events", "array-contains", sportEvent);
+  db.collection(collection)
+    .where("sex", "==", "F") //change to sport, gender, event, etc.
+    .get()
+    .then((athList) => {
+      athList.forEach((athlete) => {
+        // console.log(athlete.data().name);
+      });
+    })
+    .catch((error) => {
+      console.log("Error getting athletes: ", error);
+    });
+  // db.collection(collection)
+  //   .get()
+  //   .then((snap) => {
+  //     var i = 1;
+
+  //     snap.forEach((doc) => {
+  //       // iterate thru each doc
+
+  //       // set some variable to data from database document
+  //       var name = doc.data().name;
+  //       var age = doc.data().age;
+
+  //       // clone the template
+  //       let newcard = cardTemplate.content.cloneNode(true);
+
+  //       // update elements of the clone
+  //       newcard.querySelector(".athlete-card-name").innerHTML = name;
+  //       newcard.querySelector(".athlete-card-age").innerHTML = age;
+
+  //       // attach to gallery
+  //       document.getElementById(collection + "-go-here").appendChild(newcard);
+  //       i++;
+  //     });
+  //   });
+}
+
+displayCards("athletes");
 
 function diplayAthletes() {
   eventSelection = $("#event-selection").val();
--- scripts/sports-events.js
+++ scripts/sports-events.js
@@ -1,51 +1,50 @@
-const urlParams = new URLSearchParams(window.location.search);
-const currSport = urlParams(sport);
+// page URL should look something like:
+// /sports-events.html?sport=alpine-skiing
 
-function displayCards(collection, sport) {
+function displayCards() {
   let cardTemplate = document.getElementById("eventCardTemplate");
-
+  const urlParams = new URLSearchParams(window.location.search);
+  const currSport = urlParams.get("sport");
   // use currSport to query firestore and get all events for this sport
   // to populate templates with
-
-  db.collection(collection)
+  db.collection("sports")
+    .doc(currSport)
     .get()
-    .then((snap) => {
-      var i = 1;
-      snap.forEach((doc) => {
-        // iterate thru each doc
-
-        // set some variable to data from database document
-        // var title = doc.data().name;
-
-        // clone the template
-        let newcard = cardTemplate.content.cloneNode(true);
-
-        // update elements of the clone
-        // newcard.querySelector(".card-title").innerHTML = title;
-
-        
-        // give unique ids to all elements for future use
-        // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
-
-        // attach to gallery
-        document.getElementById(collection + "-go-here").appendChild(newcard);
-        
-        // i++;
-      });
+    .then((doc) => {
+      console.log("Men's");
+      console.log(doc.data()["Men's"]);
+      console.log("Women's");
+      console.log(doc.data()["Women's"]);
     });
-    // document.querySelector(".card").addEventListener("click", gotoEventsPage);
-    // document.querySelector("button").onclick = function () {console.log("hey")};
+
+  // db.collection(collection)
+  //   .get()
+  //   .then((snap) => {
+  //     var i = 1;
+  //     snap.forEach((doc) => {
+  //       // iterate thru each doc
+
+  //       // set some variable to data from database document
+  //       // var title = doc.data().name;
+
+  //       // clone the template
+  //       let newcard = cardTemplate.content.cloneNode(true);
+
+  //       // update elements of the clone
+  //       // newcard.querySelector(".card-title").innerHTML = title;
+
+  //       // give unique ids to all elements for future use
+  //       // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
+
+  //       // attach to gallery
+  //       document.getElementById(collection + "-go-here").appendChild(newcard);
+
+  //       // i++;
+  //     });
+  // });
+  // document.querySelector(".card").addEventListener("click", gotoEventsPage);
+  // document.querySelector("button").onclick = function () {console.log("hey")};
 }
 
 // first figure out how to read the events from firestore
-// displayCards("sports-events");
-
-function gotoAthletesPage() {
-  console.log("hey");
-  window.location.href = "athletes.html?sportevent=" + sportevent;
-}
-
-function setup() {
-  $("button").on("click", gotoAthletesPage);
-}
-$(document).ready(setup);
+displayCards();
