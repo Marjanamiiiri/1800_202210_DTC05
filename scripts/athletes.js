45180a707b298604ee570c9e7fdde9bb9858d04b
tgrigoruk <97200544+tgrigoruk@users.noreply.github.com>
2022-03-09 18:05:20 -0800

    got athletes page started

---
+++ scripts/athletes.js
@@ -0,0 +1,28 @@
+function displayCards(collection) {
+    let cardTemplate = document.getElementById("athleteCardTemplate");
+
+    db.collection(collection).get()
+        .then(snap => {
+            var i = 1;
+            snap.forEach(doc => { //iterate thru each doc
+
+                // set some variable to data from database document
+                var name = doc.data().name;
+                var age = doc.data().age;
+                
+                // clone the template
+                let newcard = cardTemplate.content.cloneNode(true);
+
+                //update elements of the clone
+                newcard.querySelector('.athlete-card-name').innerHTML = name;
+                newcard.querySelector('.thlete-card-age').innerHTML = age;
+
+
+                // attach to gallery
+                document.getElementById(collection + "-go-here").appendChild(newcard);
+                i++;
+            })
+        })
+}
+
+displayCards("athletes");
