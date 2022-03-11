4f5b934e3122551848256d03d596723a1acfe10a
tgrigoruk <97200544+tgrigoruk@users.noreply.github.com>
2022-03-10 21:12:55 -0800

    athletes page populates from DB

--- scripts/athletes.js
+++ scripts/athletes.js
@@ -3,56 +3,55 @@
 function getEventInfo() {
   const urlParams = new URLSearchParams(window.location.search);
   return {
-    sport: urlParams.get("sport"),
+    sport: toTitleCase(urlParams.get("sport").replace("-", " ")),
     gender: urlParams.get("gender"),
-    event: urlParams.get("sportevent"),
+    event: urlParams.get("event"),
   };
 }
 
 function displayCards(collection) {
   eventInfo = getEventInfo();
+  // console.log(eventInfo);
   document.getElementById("athletes-title").innerHTML =
-    "Competitors in " + eventInfo.gender + " " + eventInfo.sportevent;
+    "Competitors in " + eventInfo.gender + " " + eventInfo.event;
 
   let cardTemplate = document.getElementById("athleteCardTemplate");
   // var athletesRef = db.collection(collection);
   // use currEvent to query firestore and get all athletes for this event
   //  var query = athletesRef.where("events", "array-contains", sportEvent);
   db.collection(collection)
-    .where("sex", "==", "F") //change to sport, gender, event, etc.
+    .where("sport", "==", eventInfo.sport)
+    // .where("genders", "==", eventInfo.gender)
+    // .where("events", "==", eventInfo.event)
     .get()
-    .then((athList) => {
-      athList.forEach((athlete) => {
-        // console.log(athlete.data().name);
+    .then((snap) => {
+      snap.forEach((doc) => {
+        console.log(doc.data().name);
+        var name = doc.data().name;
+        var age = doc.data().age;
+        var pic = "./images/athletes/women_outline.png";
+        if (doc.data().sex == "M") {
+          pic = "./images/athletes/men_outline.png";
+        }
+
+        // clone the template
+        let newcard = cardTemplate.content.cloneNode(true);
+
+        // update elements of the clone
+        // newcard.querySelector("a").href = `./athlete-bio.html?id=${doc.id}`;
+        newcard.querySelector(".card-image").src = pic;
+        newcard.querySelector(".card-image").alt = name;
+        newcard.querySelector(".athlete-card-name").innerHTML = name;
+        newcard.querySelector(".athlete-card-age").innerHTML = age;
+
+        newcard.querySelector(".card").setAttribute("id", doc.id);
+        // attach to gallery
+        document.getElementById(collection + "-go-here").appendChild(newcard);
       });
     })
     .catch((error) => {
       console.log("Error getting athletes: ", error);
     });
-  // db.collection(collection)
-  //   .get()
-  //   .then((snap) => {
-  //     var i = 1;
-
-  //     snap.forEach((doc) => {
-  //       // iterate thru each doc
-
-  //       // set some variable to data from database document
-  //       var name = doc.data().name;
-  //       var age = doc.data().age;
-
-  //       // clone the template
-  //       let newcard = cardTemplate.content.cloneNode(true);
-
-  //       // update elements of the clone
-  //       newcard.querySelector(".athlete-card-name").innerHTML = name;
-  //       newcard.querySelector(".athlete-card-age").innerHTML = age;
-
-  //       // attach to gallery
-  //       document.getElementById(collection + "-go-here").appendChild(newcard);
-  //       i++;
-  //     });
-  //   });
 }
 
 displayCards("athletes");
@@ -60,6 +59,16 @@ displayCards("athletes");
 function diplayAthletes() {
   eventSelection = $("#event-selection").val();
   // console.log(eventSelection);
+}
+
+function toTitleCase(str) {
+  return str
+    .toLowerCase()
+    .split(" ")
+    .map(function (word) {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    })
+    .join(" ");
 }
 
 function setup() {
