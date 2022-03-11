a7953055628c3990d3211479edf94f5297f08089
tgrigoruk <97200544+tgrigoruk@users.noreply.github.com>
2022-03-10 20:28:32 -0800

    sports-events page populates based on sport

--- scripts/sports-events.js
+++ scripts/sports-events.js
@@ -2,49 +2,22 @@
 // /sports-events.html?sport=alpine-skiing
 
 function displayCards() {
-  let cardTemplate = document.getElementById("eventCardTemplate");
   const urlParams = new URLSearchParams(window.location.search);
   const currSport = urlParams.get("sport");
+  $("#sport-heading").text(currSport.toUpperCase());
   // use currSport to query firestore and get all events for this sport
   // to populate templates with
   db.collection("sports")
     .doc(currSport)
     .get()
     .then((doc) => {
-      console.log("Men's");
-      console.log(doc.data()["Men's"]);
-      console.log("Women's");
-      console.log(doc.data()["Women's"]);
+      doc.data()["Women's"].forEach(event => {
+        $("#womens-events").append(`<div class="event"><a href="./athletes.html?sport=${currSport}&gender=Women%27s&event=${event}">${event}</a></div>`);
+      });
+      doc.data()["Men's"].forEach(event => {
+        $("#mens-events").append(`<div class="event"><a href="./athletes.html?sport=${currSport}&gender=Men%27s&event=${event}">${event}</a></div>`);
+      });
     });
-
-  // db.collection(collection)
-  //   .get()
-  //   .then((snap) => {
-  //     var i = 1;
-  //     snap.forEach((doc) => {
-  //       // iterate thru each doc
-
-  //       // set some variable to data from database document
-  //       // var title = doc.data().name;
-
-  //       // clone the template
-  //       let newcard = cardTemplate.content.cloneNode(true);
-
-  //       // update elements of the clone
-  //       // newcard.querySelector(".card-title").innerHTML = title;
-
-  //       // give unique ids to all elements for future use
-  //       // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
-
-  //       // attach to gallery
-  //       document.getElementById(collection + "-go-here").appendChild(newcard);
-
-  //       // i++;
-  //     });
-  // });
-  // document.querySelector(".card").addEventListener("click", gotoEventsPage);
-  // document.querySelector("button").onclick = function () {console.log("hey")};
 }
 
-// first figure out how to read the events from firestore
 displayCards();
