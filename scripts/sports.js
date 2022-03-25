67331fd97c0131c2004674121552f2167d9742d1
tgrigoruk <97200544+tgrigoruk@users.noreply.github.com>
2022-03-24 22:32:17 -0700

    corrected sports names (title case)

--- scripts/sports.js
+++ scripts/sports.js
@@ -5,28 +5,23 @@ function displayCards(collection) {
     .get()
     .then((snap) => {
       var i = 1;
-      snap.forEach((doc) => { // iterate thru each doc
+      snap.forEach((doc) => {
+        // iterate thru each doc
         // set some variable to data from database document
-        var title = doc.id;
-        var photo = "./images/sports/" + doc.data().image;
+        var title = doc.data().name;
+        var photo = "./images/sports/" + doc.id + ".svg";
 
         // clone the template
         let newcard = cardTemplate.content.cloneNode(true);
 
         // update elements of the clone
+        newcard.querySelector(".card-title").innerHTML = title;
+        newcard.querySelector(".card-image").src = photo;
         newcard.querySelector(".card-block").href =
           "sports-events.html?sport=" + doc.id;
-        newcard.querySelector(".card-title").innerHTML = title;
-        newcard.querySelector(".card-image").src =
-          "./images/sports/" + doc.id + ".svg";
-
-        // give unique ids to all elements for future use
-        // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
 
         // attach to gallery
         document.getElementById(collection + "-go-here").appendChild(newcard);
-
-        // i++;
       });
     });
 }
