82ac8781bce5f4773b5cf73c76237b1febffefac
tgrigoruk <terence.grigoruk@gmail.com>
2022-03-24 20:09:27 -0700

    fixed events.hteml + athletes.html displaying

--- scripts/athletes.js
+++ scripts/athletes.js
@@ -3,23 +3,22 @@
 function getEventInfo() {
   const urlParams = new URLSearchParams(window.location.search);
   return {
-    sport: toTitleCase(urlParams.get("sport").replace("-", " ")),
+    sport: urlParams.get("sport"),
+    sporttitle: toTitleCase(urlParams.get("sport").replace("-", " ")),
     gender: urlParams.get("gender"),
     event: urlParams.get("event"),
   };
 }
 
-var currentUser;
 function getCurrentUser() {
   firebase.auth().onAuthStateChanged((user) => {
-    // Check if user is loged in:
     if (user) {
-      // console.log(user.uid); //display user who just logged in
-      currentUser = db.collection("users").doc(user.uid); //get doc associated with user
+      currentUser = db.collection("users").doc(user.uid);
       currentUser.get().then((userDoc) => {
         var user_Name = userDoc.data().name;
-        // console.log(user_Name);
-        $("#name-goes-here").text(user_Name); //set text of html element to username
+        // console.log("athletes.js: " + user_Name + ", " + user.uid);
+        displayCards("athletes");
+        // updateAthletesAddedStatus();
       });
     } else {
       console.log("No user is logged in.");
@@ -28,74 +27,66 @@ function getCurrentUser() {
 }
 getCurrentUser();
 
+// console.log("current user is " + currentUser);
+
 function displayCards(collection) {
   // "athletes"
   eventInfo = getEventInfo();
-  console.log(eventInfo);
+  // console.log(eventInfo);
 
-  // console.log(currentUser.id);
   document.getElementById("athletes-title").innerHTML =
     "Competitors in " + eventInfo.gender + " " + eventInfo.event;
 
+  console.log("current user is " + currentUser);
   //get user team info to update "added" status on athlete cards
-  // var userTeam;
-  // currentUser.get().then((userDoc) => {
-  //   userTeam = userDoc.data().team; // array of athlete ids
-  //   userTeamName = userDoc.data().teamname; // string
-  //   console.log("Team " + userTeamName + " contains " + userTeam);
-  // });
+  var userTeam;
+  currentUser.get().then((userDoc) => {
+    userTeam = userDoc.data().team; // array of athlete ids
+    userTeamName = userDoc.data().teamname; // string
+    console.log("Team " + userTeamName + " contains " + userTeam);
+  });
 
-  let cardTemplate = document.getElementById("athleteCardTemplate");
-  // var athletesRef = db.collection(collection);
-  // use currEvent to query firestore and get all athletes for this event
-  //  var query = athletesRef.where("events", "array-contains", sportEvent);
-
-  fakeList = [60215, 38381, 15104, 10926];
-  // get array of athletes contained in event, forEach get athlete doc and make card
-  db.collection(collection)
-    .where("age", "<", 30)
-    // .where(admin.firestore.FieldPath.documentId(), "in", userTeam)
-    // .where("genders", "==", eventInfo.gender)
-    // .where("events", "==", eventInfo.event)
+  db.collection(`sports/${eventInfo.sport}/${eventInfo.gender}`)
+    .doc(eventInfo.event)
     .get()
-    .then((snap) => {
-      snap.forEach((doc) => {
-        console.log(doc.data().name);
-        var name = doc.data().name;
-        var age = doc.data().age;
-        var pic = "./images/athletes/women_outline.png";
-        if (doc.data().sex == "F") {
-          pic = "./images/athletes/men_outline.png";
-        }
+    .then(eventDoc => {
+      var athletesInEvent = eventDoc.data().athletes;
+      athletesInEvent.forEach(a => {
+        // console.log(a);
+        db.collection("athletes")
+          .doc(a + '')
+          .get()
+          .then(athleteDoc => {
+            if (!(athleteDoc.exists)) { console.log(a, 'does not exist'); return; }
+            // console.log(athleteDoc);
+            var name = athleteDoc.data().name;
+            var age = athleteDoc.data().age;
+            var pic = "./images/athletes/women_outline.png";
+            if (athleteDoc.data().sex == "M") {
+              pic = "./images/athletes/men_outline.png";
+            }
 
-        // clone the template
-        let newcard = cardTemplate.content.cloneNode(true);
+            // clone the template
+            let newcard = athleteCardTemplate.content.cloneNode(true);
+            // update elements of the clone
+            newcard.querySelector(".card-image").src = pic;
+            newcard.querySelector(".card-image").alt = name;
+            newcard.querySelector(".athlete-card-name").innerHTML = name;
+            newcard.querySelector(".athlete-card-age").innerHTML = age;
+            newcard.querySelector(".card").setAttribute("id", athleteDoc.id);
 
-        // update elements of the clone
-        // newcard.querySelector("a").href = `./athlete-bio.html?id=${doc.id}`;
-        newcard.querySelector(".card-image").src = pic;
-        newcard.querySelector(".card-image").alt = name;
-        newcard.querySelector(".athlete-card-name").innerHTML = name;
-        newcard.querySelector(".athlete-card-age").innerHTML = age;
+            newcard.querySelector("a").href = "./athlete-info.html?id=" + athleteDoc.id;
+            newcard.querySelector("i").onclick = () => addToTeam(currentUser, athleteDoc.id);
+            // if (userTeam.includes(athleteDoc.id)) {
+            //   newcard.querySelector("i").innerHTML = "bookmark";
+            // }
 
-        newcard.querySelector(".card").setAttribute("id", doc.id);
-
-        // newcard.querySelector("i").onclick = () => addToTeam(doc.id);
-        // if (userTeam.includes(doc.id)) {
-        //   newcard.querySelector("i").innerHTML = "bookmark";
-        // }
-        // newcard.querySelector(".read-more").href =
-        //   "athleteinfo.html?id=" + doc.id; // + "&event=" + getEventInfo.event;
-
-        // attach to gallery
-        document.getElementById(collection + "-go-here").appendChild(newcard);
+            document.getElementById(collection + "-go-here").appendChild(newcard);
+          });
       });
-    })
-    .catch((error) => {
-      console.log("Error getting athletes: ", error);
     });
 }
-displayCards("athletes");
+
 
 function diplayAthletes() {
   eventSelection = $("#event-selection").val();
@@ -112,11 +103,11 @@ function toTitleCase(str) {
     .join(" ");
 }
 
-function addToTeam(athlete) {
+function addToTeam(currentUser, athlete) {
   currentUser.get().then((userDoc) => {
-    //get the user name
-    // console.log(userDoc.data().bookmarks);
-    if (userDoc.data().bookmarks.includes(athlete)) {
+    user = user.data()
+    console.log(user.name, user.bookmarks);
+    if (user.data().bookmarks.includes(athlete)) {
       currentUser
         .set(
           {
--- scripts/sports-events.js
+++ scripts/sports-events.js
@@ -6,70 +6,22 @@ function displayCards() {
   const currSport = urlParams.get("sport");
   $("#sport-heading").text(currSport.toUpperCase());
 
-  // for (gender of ["Women's", "Men's", "Mixed"]) {}
-  // let lowercaseGender = gender.replace("'", "").toLowerCase();
-  // console.log(lowercaseGender);
-  // $(`${lowercaseGender}-events`).append(`<h2>${gender}</h2>`);
-  let gender = "Women's";
-  db.collection("sports/" + currSport + `/${gender}`)
-    .get()
-    .then((snap) => {
-      $(`#${lowerCaseGender(gender)}-events`).append(`<h2>${gender}</h2>`);
-      snap.forEach((eventSnap) => {
-        currEvent = eventSnap.id;
-        $(`#${lowerCaseGender(gender)}-events`).append(
-          `<div class="event"><a href="./athletes.html?sport=${currSport}&gender=${gender}&event=${currEvent}">${currEvent}</a></div>`
-        );
+  ["Women's", "Men's", "Mixed"].forEach(gender => {
+    db.collection("sports/" + currSport + `/${gender}`)
+      .get()
+      .then((snap) => {
+        if (snap.size > 0) {
+          $('#gender-container').append(`<h2>${gender}</h2>`);
+          snap.forEach((eventSnap) => {
+            currEvent = eventSnap.id;
+            // console.log(currEvent)
+            $('#gender-container').append(
+              `<div class="event"><a href="./athletes.html?sport=${currSport}&gender=${gender}&event=${currEvent}">${currEvent}</a></div>`
+            );
+          });
+        }
       });
-    });
-  // gender = "Men's";
-  // db.collection("sports/" + currSport + `/${gender}`)
-  //   .get()
-  //   .then((snap) => {
-  //     $(`#${lowerCaseGender(gender)}-events`).append(`<h2>${gender}</h2>`);
-  //     snap.forEach((eventSnap) => {
-  //       currEvent = eventSnap.id;
-  //       $(`#${lowerCaseGender(gender)}-events`).append(
-  //         `<div class="event"><a href="./athletes.html?sport=${currSport}&gender=${gender}&event=${currEvent}">${currEvent}</a></div>`
-  //       );
-  //     });
-  //   });
-  // gender = "Mixed";
-  // db.collection("sports/" + currSport + `/${gender}`)
-  //   .get()
-  //   .then((snap) => {
-  //     $(`#${lowerCaseGender(gender)}-events`).append(`<h2>${gender}</h2>`);
-  //     snap.forEach((eventSnap) => {
-  //       currEvent = eventSnap.id;
-  //       $(`#${lowerCaseGender(gender)}-events`).append(
-  //         `<div class="event"><a href="./athletes.html?sport=${currSport}&gender=${gender}&event=${currEvent}">${currEvent}</a></div>`
-  //       );
-  //     });
-  //   });
-
-  // .get()
-  // .then((sportSnap) => {
-  //   console.log(sportSnap);
-  //   sportSnap.forEach((sportDoc) => {
-  //     console.log(sportDoc.data());
-  //     sportDoc.listCollections().then((querySnapshot) => {
-  //       querySnapshot.forEach((collection) => {
-  //         console.log("collection: " + collection.id);
-  //       });
-  //     });
-  //     // console.log(doc.data().athletes);
-  //   });
-  // });
-
-  // .doc(currSport)
-  // .listCollections()
-  // .then((subCollections) => {
-  //   subCollections.forEach((subCollection) => {
-  //     subCollection.get().then((athletes) => {
-  //       console.log(athletes);
-  //     });
-  //   });
-  // });
+  });
 
   // original way
   // doc.data()["Women's"].forEach(event => {
