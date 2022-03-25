7157d79849aebb4c496087774243b973be1d513d
WallaceTrinh <WTrinh1@my.bcit.ca>
2022-03-24 23:31:08 -0700

    tried adding teams within compete.html and compete.js files

--- scripts/compete.js
+++ scripts/compete.js
@@ -1,52 +1,53 @@
 //For the implementation of Groups, allowing input
 
 function populateInfo() {
-  firebase.auth().onAuthStateChanged(user => {
-      // Check if user is signed in:
-      if (user) {
+  firebase.auth().onAuthStateChanged((user) => {
+    // Check if user is signed in:
+    if (user) {
+      //go to the correct user document by referencing to the user uid
+      currentUser = db.collection("users").doc(user.uid);
+      //get the document for current user.
+      currentUser.get().then((userDoc) => {
+        //get the data field of the user for groups they registered in
+        var userName = userDoc.data().name;
+        var userTeam = userDoc.data().team;
 
-          //go to the correct user document by referencing to the user uid
-          currentUser = db.collection("users").doc(user.uid)
-          //get the document for current user.
-          currentUser.get()
-              .then(userDoc => {
-                  //get the data field of the user for groups they registered in
-                  var GroupNameInput = userDoc.data().GroupName;
-
-                  //if the data fields are not empty, then write them in to the form.
-                  if (GroupNameInput != null) {
-                      document.getElementById("GroupNameInput").value = GroupNameInput;
-                  }
-              })
-      } else {
-          // No user is signed in.
-          console.log("No user is signed in");
-      }
+        //if the data fields are not empty, then write them in to the form.
+        if (userName != null) {
+          document.getElementById("nameInput").value = userName;
+        }
+        if (userTeam != null) {
+          document.getElementById("teamInput").value = userTeam;
+        }
+      });
+    } else {
+      // No user is signed in.
+      console.log("No user is signed in");
+    }
   });
 }
 
-//call the function to run it 
+//call the function to run it
 populateInfo();
 
-function editUserInfo() {
-  document.getElementById("personalInfoFields").disabled = false
-
-}
-editUserInfo();
-
+//To save the data that the user has input and add it to the firebase
 function saveUserInfo() {
-  userName = document.getElementById('GroupNameInput').value;
+  userName = document.getElementById("nameInput").value;  //takes the value that was given in nameInput
+  userTeam = document.getElementById("teamInput").value;  //takes the value that was given in teamInput
 
   currentUser.update({
-          group: groupName,
-
-      })
-      .then(() => {
-          console.log("Document successfully updated!");
-      })
+      name: userName,
+      team: userteam,
+    })
+    .then(() => {
+      console.log("Team successfully added!");
+    });
 
   //Enable the form fields
-  document.getElementById('personalInfoFields').disabled = true;
+  document.getElementById("teamlInfoFields").disabled = true;
 }
 
-  jQuery(document).ready(setup);
+//call the function to run it
+saveUserInfo();
+
+jQuery(document).ready(setup);
