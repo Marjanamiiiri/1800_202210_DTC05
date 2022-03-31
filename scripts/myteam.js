bb23816fa9e9c37b91da11c4693d99a19ce44e04
tgrigoruk <97200544+tgrigoruk@users.noreply.github.com>
2022-03-31 12:25:11 -0700

    fixed myteam.js populating table

--- scripts/myteam.js
+++ scripts/myteam.js
@@ -1,5 +1,5 @@
-import * as firebase from 'firebase/app';
-const db = firebase.firestore();
+// import * as firebase from 'firebase/app';
+// const db = firebase.firestore();
 
 var currentUser;
 function getCurrentUser() {
@@ -22,89 +22,29 @@ getCurrentUser();
 function displayTeam(user) {
   var userTeam;
   user.get().then((userDoc) => {
-    //get user team info
     userTeam = userDoc.data().team; // array of athlete ids (numbers)
     userTeamName = userDoc.data().teamname; // string
-    console.log("Team " + userTeamName + " contains " + userTeam);
+    // console.log("Team " + userTeamName + " contains " + userTeam);
 
-    userTeam.forEach((athletesID) => {
-      // console.log(typeof athleteID, athleteID);
+    userTeam.forEach((athleteID) => {
+      athleteID = athleteID.toString();
       db.collection("athletes")
-        .get(athletesID + "")
-        .then((athletes) => {
-          console.log(athletes.name);
-          document
-            .getElementById("table-body")
-            .append(
-              `<tr><td>${athletes.name}</td><td>${athletes.sport}</td><td>${athletes.team}</td></tr>`
-            );
+        .doc(athleteID.toString())
+        .get()
+        .then((athleteDoc) => {
+          if (!athleteDoc.exists) {
+            console.log(athleteID, "does not exist");
+            return;
+          }
+          athleteName = athleteDoc.data().name;
+          athleteSport = athleteDoc.data().sport;
+          athleteCountry = athleteDoc.data().team;
+          // athleteCountry = athleteDoc.data().points;
+          document.getElementById(
+            "table-body"
+          ).innerHTML += `<tr><td>${athleteName}</td><td>${athleteSport}</td><td>${athleteCountry}</td></tr>`;
         });
     });
   });
 }
-displayTeam();
-
-
-// This function is used to populate the information into the table
-function populateAthleteInfo() {
-  firebase.auth().onAuthStateChanged(user => {
-      // Check if user is signed in:
-      if (user) {
-
-          //go to the correct athletes document by referencing to the athletes id
-          currentUser = db.collection("athletes").doc(athletesID)
-          //get the document for current user.
-          currentUser.get()
-              .then(athletesDoc => {
-                  //get the data fields of the athletes
-                  var athleteName = athletesDoc.data().name;
-                  var athleteSport = athletesDoc.data().sport;
-                  // This is commented out because points do not exist at the moment
-                  // var athletePoints = athletesDoc.data().points;
-
-                  //if the data fields are not empty, then write them in to the form.
-                  if (athleteName != null) {
-                      document.getElementById("athleteName").value = athleteName;
-                  }
-                  if (athleteSport != null) {
-                      document.getElementById("athleteSport").value = athleteSport;
-                  }
-                  // This is commented out because points do no exist at the moment
-                  // if (athletePoints != null) {
-                  //     document.getElementById("athletePoints").value = athletePoints;
-                  // }
-              })
-      } else {
-          // No user is signed in.
-          console.log ("No user is signed in");
-      }
-  });
-}
-
-//call the function to run it 
-populateAthleteInfo();
-
-
-function populateAthleteDynamically(){
-  let athleteTableTemplate = document.getElementById("athleteCardTemplate");
-  let tablebody = document.getElementById("table-body");
-
-  db.collection("athletes").get()
-    .then(allathletes => {
-    allathletes.forEach(doc => {
-      var athletesName = doc.data().name;  // gets the name of the athlete
-      var athletesSport = doc.data().sport;  // gets the sport name of the athlete
-      var athletesPoints = doc.data().points;  // gets the points from that one athlete
-      let fillAthleteTable = athleteTableTemplate.content.cloneNode(true);
-      testAthlete.querySelector('athlete-card-name').innerHTML = athletesName;
-      testAthlete.querySelector('athlete-card-sport').innerHTML = athletesSport;
-      testAthlete.querySelector('athlete-card-points').innerHTML = athletesPoints;
-      tablebody.appendChild(fillAthleteTable);
-    })
-})
-}
-
-populateAthleteDynamically();
-
-
-
+// displayTeam();
