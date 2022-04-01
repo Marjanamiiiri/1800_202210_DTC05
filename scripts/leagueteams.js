df18fb6fb39b47c586116e27cc9e1b01cfd6e961
tgrigoruk <97200544+tgrigoruk@users.noreply.github.com>
2022-03-31 19:01:12 -0700

    leagueteams function gets teams in league

--- scripts/leagueteams.js
+++ scripts/leagueteams.js
@@ -1,97 +1,85 @@
+var currentUser;
+function getCurrentUser() {
+  firebase.auth().onAuthStateChanged((user) => {
+    // Check if user is loged in:
+    if (user) {
+      currentUser = db.collection("users").doc(user.uid); //get doc associated with user
+      currentUser.get().then((userDoc) => {
+        const urlParams = new URLSearchParams(window.location.search);
+        const league = urlParams.get("league");
+        getUsers(league);
+      });
+    } else {
+      console.log("No user is logged in.");
+    }
+  });
+}
+getCurrentUser();
 
-// This is to be moved over to the leagues.js
-function leagueMoveURL() {  //Add sortkey into the parameter
+function getUsers(league) {
   db.collection("leagues")
-  .get()
-  .then(allusers => {
-      allusers.forEach(doc => {  // forEach goes through each doc
-          var leagueName = doc.data().league; //gets the name of the key field
-          let leagueExpand = leagueNameclickable***.content.cloneNode(true);
-          leagueExpand.querySelector('.list-group-item').href = "leagues.html?leagueName="+leagueName;
+    .doc(league)
+    .get()
+    .then((doc) => {
+      // list of users with team in current league
+      const leagueUsers = doc.data().users;
+      // TODO put current user FIRST
+      
 
-
-// This is to grab other user's information and populate in the accordian
-var otherUser;
-function getOtherUser() {
-  FirebaseDatabase.DefaultInstance
-  .GetArray("leagues")
-  .GetValueAsync().ContinueWithOnMainThread(task => {
-    if (task.IsFaulted) {
-      console.log("Other user's data does not exist")
-    }
-    else if (task.IsCompleted) {
-      DataSnapshot = task.Result;
-      if (Users) {
-        otherUser = db.collection("users").doc(Users.uid).userDoc.data().league; //get doc associated with user
-        otherUser.get().then((userDoc) => {
-          var user_League = userDoc.data().league;
-          console.log("main.js: " + user_League + ", " + user.uid);
-          displayTeam(otherUser);
-      // Do something with snapshot...
-    }
-  );
-    };
-  }
-getOtherUser();
-
-
-
-
-// var otherUser;
-// function getOtherUser() {
-//   firebase.auth().onAuthStateChanged((Users) => {
-//     // Check if user is logged in:
-//     if (Users) {
-//       otherUser = db.collection("users").doc(Users.uid).userDoc.data().league; //get doc associated with user
-//       otherUser.get().then((userDoc) => {
-//         var user_League = userDoc.data().league;
-//         console.log("main.js: " + user_League + ", " + user.uid);
-//         displayTeam(otherUser);
-//       });
-//     } else {
-//       console.log("Cannot grab other user's information");
-//     }
-//   });
-// }
-// getOtherUser();
-
-
-// This is for other user's data to be populated
-// function displayLeagueOthers(user) {
-    var otherUserTeam;
-    user.get().then((userDoc) => {
-      otherUserTeam = userDoc.data().team; // array of athlete ids (numbers) of other users
-      otherUserTeamName = userDoc.data().teamname; // string
-      // console.log("Team " + userTeamName + " contains " + userTeam);
-  
-      userTeam.forEach((athleteID) => {
-        athleteID = athleteID.toString();
-        db.collection("athletes")
-          .doc(athleteID.toString())
+      leagueUsers.forEach((user) => {
+        db.collection("users")
+          .doc(user)
           .get()
-          .then((athleteDoc) => {
-            if (!athleteDoc.exists) {
-              console.log(athleteID, "does not exist");
-              return;
-            }
-            userTeamName = userDoc.data().teamname;
-            userLeague = userDoc.data().league;
-            userAthletes = userDoc.data().team;
-            // athleteCountry = athleteDoc.data().points;
+          .then((userDoc) => {
 
-            // This is to grab the other user's team name and gives the accordian the other user's team name
-            document.getElementById(
-              "other-user-team"
-            ).innerHTML += `${userTeamName}`;
+            // in accordian header
+            const teamName = userDoc.data().teamname;
+            const userName = userDoc.data().name;
+            let points = userDoc.data().points;
+            // accordianButtonText = teamName + " - " + points + "pts";
 
-            // This document populates the table within this accordian
-            document.getElementById("table-body").innerHTML += `<tr>
-            <td>${athleteName}</td>
-            <td>${athleteSport}</td>
-            <td>${"#"}</td>
-            <td><i class="material-icons add-button" id="add-${athleteID}">remove_circle_outline</i></td>
-            </tr>`;
+            // populate table with above
+
+
+
+
+
+
+
+
+
+
+            // in accordian body (table)
+            // let points = 0;
+            // const team = userDoc.data().team;
+            // team.forEach((athleteID) => {
+            //   athleteID = athleteID.toString();
+            //   db.collection("athletes")
+            //     .doc(athleteID.toString())
+            //     .get()
+            //     .then((athleteDoc) => {
+            //       if (!athleteDoc.exists) {
+            //         console.log(athleteID, "does not exist");
+            //         return;
+            //       }
+            //       athleteName = athleteDoc.data().name;
+            //       athleteSport = athleteDoc.data().sport;
+            //       athleteCountry = athleteDoc.data().noc; 
+            //       // athleteItem = athleteName + " " + athleteSport;
+                  
+            //       // document.getElementById("table-body").innerHTML += `<tr>
+            //       //   <td>${athleteName}</td>
+            //       //   <td>${athleteSport}</td>
+            //       //   <td>${"#"}</td>
+            //       //   </tr>`;
+
+
+            //       // calculate total points based on athletes' points
+            //       // athleteCountry = athleteDoc.data().points;
+            //       // points += athleteDoc.data().points;
+            //     });
+            // });
           });
       });
     });
-  }
+}
